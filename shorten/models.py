3ea5ff913a76dbbe620235dc8e59921feedeb9c9
Fela Winkelmolen <felawinkelmolen@gmail.com>
2013-07-25 21:27:18 +0200

    implement the actual shortener
    code that transforms a url to a short string and stores the
    association in the db

--- shorten/models.py
+++ shorten/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+import base64
+import struct
 
 
 # url will be stored
 class Url(models.Model):
     # urls longer than 2083 char don't work in some browsers anyway
     MAX_LENGTH = 2083
-    url = models.CharField(max_length=MAX_LENGTH)
+    url = models.CharField(max_length=MAX_LENGTH, unique=True)
     # the url will be shortened to http://domain.com/<short_string>
-    short_string = models.CharField(max_length=200)
+    short_string = models.CharField(max_length=200, unique=True)
+
+    @staticmethod
+    def shorten(url):
+        if Url.objects.filter(url=url).exists():
+            return Url.objects.get(url=url).short_string
+        else:
+            new = Url(url=url)
+            new.save()
+            new.short_string = Url.int_to_string(new.id)
+            new.save()
+            return new.short_string
+
+    @staticmethod
+    def int_to_string(int):
+        # transform int in bytestring
+        bytes = struct.pack(">i", int).lstrip(chr(0)) or chr(0)
+        # convert the bytestring string to characters and numbers
+        return base64.b32encode(bytes).strip('=').lower()
--- shorten/views.py
+++ shorten/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from models import Url
 
 # Create your views here.
 def index(request):
-    return shortened(request, 'http://test.com')
-    #return render(request, 'index.html')
+    url = request.GET.get('url', '')
+    if url == '':
+        return render(request, 'index.html')
+    else:
+        return shortened(request, url)
 
 def shortened(request, url):
-    return render(request, 'shortened.html', {'short_url':url})
+    short_url = request.get_host() + '/' + Url.shorten(url)
+    return render(request, 'shortened.html', {'short_url':short_url})
